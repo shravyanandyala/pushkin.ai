00831168587a6655cedef79dcd5a83351eb9ddcd
Shravya Nandyala <shravya.nandyala@gmail.com>
2022-10-31 12:50:31 -0400

    added russian poem files, lstm code

---
+++ lstm.py
@@ -0,0 +1,81 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow.keras.utils as ku
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout, Bidirectional
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras import regularizers
+
+# Reading the text data file
+data = open('russian_5000.txt', encoding="utf8").read()
+
+# Generating the corpus by
+# splitting the text into lines
+corpus = data.lower().split("\n")
+
+# Fitting the Tokenizer on the Corpus
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(corpus)
+
+# Vocabulary count of the corpus
+total_words = len(tokenizer.word_index)
+
+print("Total Words:", total_words)
+
+# Converting the text into embeddings
+input_sequences = []
+for line in corpus:
+	token_list = tokenizer.texts_to_sequences([line])[0]
+
+	for i in range(1, len(token_list)):
+		n_gram_sequence = token_list[:i+1]
+		input_sequences.append(n_gram_sequence)
+
+max_sequence_len = max([len(x) for x in input_sequences])
+input_sequences = np.array(pad_sequences(input_sequences,
+										maxlen=max_sequence_len,
+										padding='pre'))
+predictors, label = input_sequences[:, :-1], input_sequences[:, -1]
+label = ku.to_categorical(label, num_classes=total_words+1)
+
+# Building a Bi-Directional LSTM Model
+model = Sequential()
+model.add(Embedding(total_words+1, 100,
+					input_length=max_sequence_len-1))
+model.add(Bidirectional(LSTM(150, return_sequences=True)))
+model.add(Dropout(0.2))
+model.add(LSTM(100))
+model.add(Dense(total_words+1/2, activation='relu',
+				kernel_regularizer=regularizers.l2(0.01)))
+model.add(Dense(total_words+1, activation='softmax'))
+model.compile(loss='categorical_crossentropy',
+			optimizer='adam', metrics=['accuracy'])
+print(model.summary())
+
+history = model.fit(predictors, label, epochs=100, verbose=1)
+
+seed_texts= ["Ночь, улица, фонарь, аптека,", "Я вас любил: любовь еще, быть может,", "Как грустно и все же как хочется жить,", "Двадцать первое. Ночь. Понедельник."]
+next_words_set = 25
+ouptut_text = ""
+
+for seed_text in seed_texts:
+    for next_words in next_words_set:
+        for _ in range(next_words):
+            token_list = tokenizer.texts_to_sequences([seed_text])[0]
+            token_list = pad_sequences(
+                [token_list], maxlen=max_sequence_len-1,
+            padding='pre')
+            predicted = np.argmax(model.predict(token_list,
+                                                verbose=0), axis=-1)
+            output_word = ""
+            
+            for word, index in tokenizer.word_index.items():
+                if index == predicted:
+                    output_word = word
+                    break
+                    
+            seed_text += " " + output_word
+        print(seed_text)
